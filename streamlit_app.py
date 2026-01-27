230ae5b68142a8c76ca99e6197db4d690199017d
Ryan Peters <petersryan1993@gmail.com>
2026-01-27 14:26:18 -0500

    changed icon on profile

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Define pages
 lab1 = st.Page("Lab1.py", title="Lab 1", icon=":material/description:")
-lab2 = st.Page("Lab2.py", title="Lab 2", icon=":material/summarize:", default=True)
+lab2 = st.Page("Lab2.py", title="Lab 2", icon=":material/description:", default=True)
 
 # Create navigation
 pg = st.navigation([lab1, lab2])
